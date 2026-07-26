0af08b3540dd572f1960f1b4e33a5ffbfc4a2943
TheMemeBanker <thememebanker@users.noreply.github.com>
2026-07-26 10:19:32 -0500

    attach custom domain zereblast.org

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", basePath: "/zereblast-site", assetPrefix: "/zereblast-site/", images: { unoptimized: true }, trailingSlash: true,
+  output: "export", images: { unoptimized: true }, trailingSlash: true,
   /* config options here */
 };
 
